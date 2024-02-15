ed24898642a152fcc0ad04930e8d134420e90ada
Michael Bunte <michaelwbunte@gmail.com>
2024-02-14 16:42:45 -0800

    removed unneeded pages

--- client/src/App.js
+++ client/src/App.js
@@ -29,28 +29,29 @@ navbar usage example at:
 https://github.com/sd1337/adminlte-2-react-examples/blob/a263c05df9193264ce08c7b674460d15fd3fe086/src/App.jsx#L4
 */
 
-export const SystemContext = React.createContext({system: "bluerock"});
+export const SystemContext = React.createContext({ system: "bluerock" });
 function App() {
-  const [appContext, setAppContext] = useState({system: "bluerock"});
+  const [appContext, setAppContext] = useState({ system: "bluerock" });
   const sidebar = [
-    <Item key="hello" icon="fas-home" text="Home" to="/dashboard" />,
-    <Item key="user-management" icon="fas-user" text="User Management" to="/user-management" />,
-    <Item key="data-management" icon="fas-database" text="Data Management" to="/data-management" />,
-    <Item key="detailed-dashboard" icon="fas-chart-line" text="Detailed Dashboard" to="/detailed-dashboard" />,
-    <Item key="system-management" icon="fas-tasks" text="System Management" to="/system-management" />,
-    <Item key="alert-forms" icon="fas-flag" text="Alert Forms" to="/alert-forms" />,
-    <Item key="documentation" icon="fas-book" text="Documentation" to="/documentation" />,
-    <Item key="about" icon="fas-cubes" text="About SVWaterNet" to="/about" />,
-    <Item key="faq" icon="fas-question-circle" text="FAQ and Contact" to="/faq" />,
-    <Item key="info" icon="fas-info-circle" text="Information" to="/info" />
+    <Item key="hello" icon="fas-chart-line" text="Bluerock History Dashboard" to="/detailed-dashboard" />,
+    // <Item key="user-management" icon="fas-user" text="User Management" to="/user-management" />,
+    // <Item key="data-management" icon="fas-database" text="Data Management" to="/data-management" />,
+    // <Item key="detailed-dashboard" icon="fas-chart-line" text="Detailed Dashboard" to="/detailed-dashboard" />,
+    // <Item key="system-management" icon="fas-tasks" text="System Management" to="/system-management" />,
+    // <Item key="alert-forms" icon="fas-flag" text="Alert Forms" to="/alert-forms" />,
+    // <Item key="documentation" icon="fas-book" text="Documentation" to="/documentation" />,
+    // <Item key="about" icon="fas-cubes" text="About SVWaterNet" to="/about" />,
+    // <Item key="faq" icon="fas-question-circle" text="FAQ and Contact" to="/faq" />,
+    // <Item key="info" icon="fas-info-circle" text="Information" to="/info" />
   ];
 
 
   return (
     <SystemContext.Provider value={appContext}>
-      <AdminLTE title={["WaTeR", "System"]}
-        titleShort={["W", "S"]}
+      <AdminLTE
         theme="blue-light"
+        title={["DSWT", "Tools"]}
+        titleShort={["D", "T"]}
         sidebar={sidebar}
       >
 
@@ -63,7 +64,7 @@ function App() {
             gap: "20px",
             padding: "0px 20px"
           }}>
-            <select
+            {/* <select
               value={appContext["system"]}
               onChange={(e) => { setAppContext((oldData)=>(
                 {...oldData,
@@ -83,19 +84,20 @@ function App() {
                 color='white'
               />
               <div style={{ color: "white" }}> USERNAME </div>
-            </div>
+            </div> */}
           </div>
         </Navbar.Core>
-        <DashboardPage path="/dashboard" />
-        <DataManagement path="/data-management" />
+        {/* <DashboardPage path="/dashboard" /> */}
+        <DetailedDashPage path="/" />
         <DetailedDashPage path="/detailed-dashboard" />
+        {/* <DataManagement path="/data-management" />
         <Information path="/info" />
         <About path="/about" />
         <Documentation path="/documentation" />
         <FAQnContact path="/faq" />
         <AlertForms path="/alert-forms" />
         <SystemManagement path="/system-management" />
-        <UserManagement path="/user-management" />
+        <UserManagement path="/user-management" /> */}
       </AdminLTE>
     </SystemContext.Provider>
   );
