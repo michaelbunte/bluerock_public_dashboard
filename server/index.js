268c48c590190f3a1a192e9a9e9571a8d467fe82
Michael Bunte <michaelwbunte@gmail.com>
2024-02-26 14:29:32 -0800

    Adaptive all sensors search

--- server/index.js
+++ server/index.js
@@ -3,6 +3,22 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
+function resizeArray(originalArray, newSize) {
+    if (newSize >= originalArray.length) {
+      return originalArray;
+    }
+  
+    const ratio = Math.floor(originalArray.length / newSize);
+  
+    const resizedArray = originalArray.filter(function(_, i) {
+        return i % ratio === 0;
+      })
+  
+    return resizedArray;
+  }
+  
+
+
 let sites_info;
 async function setup() {
     let site_info_query_results = await pool.query(
@@ -72,8 +88,6 @@ const query_plc_database = (
         let where_clause = "";
         if (is_date_range) {
             const { start_date, end_date } = req.params;
-            console.log(start_date);
-            console.log(end_date);
 
             const start_date_string = js_to_pg_date_string(start_date);
             const end_date_string = js_to_pg_date_string(end_date);
@@ -101,7 +115,6 @@ const query_plc_database = (
         order_by_clause,
             limit_clause, ';'].join(' ');
         
-        console.log(query_string)
 
 
         // try {
@@ -188,13 +201,9 @@ app.get(
 app.get(
     "/bluerock/adaptive_all_history/:sensor_name/:start_date/:end_date",
     async (req, res) => {
-        console.log("clicked")
         const {sensor_name, start_date, end_date } = req.params;
         const start_date_string = js_to_pg_date_string(start_date);
         const end_date_string = js_to_pg_date_string(end_date);
-
-        console.log(start_date_string);
-        console.log(end_date_string);
 
         try {
             let low_res_result = await pool.query(
@@ -207,8 +216,9 @@ app.get(
             let reformatted_low_res_result = reformat_to_simple(low_res_result, sensor_name);
             let final_result = reformatted_low_res_result;
 
-            // if range of dates is less than a month, then inject high res data
-            if (new Date(end_date) - new Date(start_date) < (60 * 60 * 24 * 1000 * 31)) {
+            // if range of dates is less than 4 months, then inject high res data
+            if (new Date(end_date) - new Date(start_date) < (4 * 60 * 60 * 24 * 1000 * 31)) {
+                console.log("high res data requested")
                 let high_res_result = await pool.query(
                     `SELECT ${sensor_name}, ` 
                     + `(plctime AT TIME ZONE 'America/Los_Angeles') `
@@ -220,7 +230,8 @@ app.get(
                     + `'${end_date_string}' `
                     + 'ORDER BY plctime ASC;'
                 );
-                    let reformatted_high_res_result = reformat_to_simple(high_res_result, sensor_name);
+                    let reformatted_high_res_result = resizeArray(
+                        reformat_to_simple(high_res_result, sensor_name), 2000);
                     let first_date = reformatted_high_res_result[0][0];
                     let last_date = reformatted_high_res_result[reformatted_high_res_result.length - 1][0];
                     let final_result_start = reformatted_low_res_result.filter((row) => row[0] < first_date);
@@ -231,9 +242,39 @@ app.get(
                         final_result_end
                     );
             }
-            
-            console.log(final_result);
             res.json(final_result);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+);
+
+app.get(
+    "/bluerock/adaptive_all_sensors/:start_date/:end_date",
+    async (req, res) => {
+        const {start_date, end_date} = req.params;
+        const start_date_string = js_to_pg_date_string(start_date);
+        const end_date_string = js_to_pg_date_string(end_date);
+        const ADAPTIVE_LIMIT_SIZE = 1000;
+        try {
+            let query = `SELECT *, (plctime AT TIME ZONE 'America/Los_Angeles') `
+            + `FROM bluerock_plc_data `
+            + `WHERE `
+                + `(plctime AT TIME ZONE 'America/Los_Angeles') >= '${start_date_string}' AND `
+                + `(plctime AT TIME ZONE 'America/Los_Angeles') <= '${end_date_string}' `
+            + 'ORDER BY plctime ASC;'
+            let result = await pool.query(query);
+            if(result.rows.length === 0) {
+                res.json(result.rows);
+                return;
+            }
+
+            let output = [];
+            let step_size = Math.max(result.rows.length / ADAPTIVE_LIMIT_SIZE, 1);
+            for(let i = 0; i < result.rows.length; i+= step_size) {
+                output.push(result.rows[Math.floor(i)]);
+            }
+            res.json(output);
         } catch (e) {
             console.error(e);
         }
@@ -257,9 +298,7 @@ app.get(
                 + `(plctime AT TIME ZONE 'America/Los_Angeles') <= '${end_date_string}' `
             + 'ORDER BY plctime ASC;'
 
-            console.log(query);
             let result = await pool.query(query);
-            console.log(result.rows[0]);
             res.json(result.rows);
         } catch (e) {
             console.error(e);
@@ -289,7 +328,6 @@ app.get(
                 ORDER BY plctime DESC limit ${range}
             ) ORDER BY plctime ASC;
             `;
-            console.log(query);
             let result = await pool.query(query);
             res.json(result.rows);
         } catch (e) {
@@ -352,7 +390,6 @@ app.get(
 app.get(
     "/bluerock/system_image/:search_time",
     async (req, res) => {
-        console.log("system image requested");
         try {
             let { search_time } = req.params;
             let reformatted_search_time = js_to_pg_date_string(search_time);
@@ -363,9 +400,7 @@ app.get(
                 +   `<= '${reformatted_search_time}' `
                 + `ORDER BY plctime DESC LIMIT 1;`;
 
-            console.log(query);
             let result = await pool.query(query);
-            console.log(result.rows)
             res.json(result.rows)
         } catch (e) {
             console.error(e);
@@ -377,7 +412,6 @@ app.get(
 app.get(
     "/bluerock/sensor_info_table",
     async ( req, res ) => {
-        console.log("sensor info table requested");
         try {
             let result = await pool.query("SELECT * FROM bluerock_sensor_info;");
             res.json(result.rows);
